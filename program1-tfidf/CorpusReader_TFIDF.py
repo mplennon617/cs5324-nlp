40c5f258f8f3d2368f48b21f8041829be1b10f43
mplennon617 <mplennon617@gmail.com>
2023-02-22 23:20:32 -0600

    IDF complete, not working yet

--- program1-tfidf/CorpusReader_TFIDF.py
+++ program1-tfidf/CorpusReader_TFIDF.py
@@ -1,6 +1,16 @@
+from nltk.corpus import CorpusReader
+from math import log
+
+# DOCUMENTATION:
+#   1. PlainTextCorpusReader:           https://www.nltk.org/_modules/nltk/corpus/reader/plaintext.html
+#   2. Corpus Reader (Parent of (1)):   https://www.nltk.org/api/nltk.corpus.reader.html?highlight=corpusreader#nltk.corpus.reader.CorpusReader
+#   3. nltk.corpus.reader (if you care):https://www.nltk.org/_modules/nltk/corpus/reader
+
+
 class CorpusReader_TFIDF:
 
-    def __init__(self, corpus, tf="raw", idf="base", stopWord="none", toStem=false, ignoreCase=true):
+    # ********************* Constructor *********************
+    def __init__(self, corpus, tf="raw", idf="base", stopWord="none", toStem=False, ignoreCase=True):
         self.corpus = corpus
         self.tf = tf
         self.idf = idf
@@ -8,40 +18,70 @@ class CorpusReader_TFIDF:
         self.toStem = toStem
         self.ignoreCase = ignoreCase
 
-    # Shared Methods
-    def fields():
+    # ******************** Shared Methods ********************
+    # TODO: fields() only exists in ToolboxCorpusReader,
+    # not PlainTextCorpusReader. Do we need to extend functionality for it?
+
+    # def fields(self, fileids=None):
+    #     return self.corpus.fields(fileids=fileids)
+
+    def raw(self, fileids=None):
+        """! Returns raw data based on given fileids 
+        (default is all docs in the corpus).
+
+        @param fields   List of fileids to read from
+        @return  Raw corpus data.
+        """
+        return self.corpus.raw(fileids=fileids)
+
+    def words(self, fileids=None):
+        """! Returns set of words from given fileids 
+        (default is all docs in the corpus).
+
+        @param fields   List of fileids to read from
+        @return  Set of words.
+        """
+        return self.corpus.words(fileids=fileids)
+
+    # *************** TF-IDF Specific Methods ***************
+    def tfidf(self, fileid, returnZero=False):
         return 0
 
-    def raw():
+    def tfidfAll(self, fileid, returnZero=False):
         return 0
 
-    def raw(fileids=[]):
+    def tfidfNew(self, words):
         return 0
 
-    def words():
+    def idf(self):
+        """! Return IDF of each term in the corpus as a dictionary. 
+        Key is the words, value is the IDF
+
+        @return  Dictionary of IDF values.
+        """
+
+        idfs = {}
+        print('We\'re in!')
+
+        # Find N
+        N = len(self.corpus.fileids())
+
+        # Calculate Document Frequencies (n_i)
+        for doc in self.corpus.fileids():
+            for word in self.words('doc'):
+                idfs[word] = idfs.get(word, default=0) + 1
+
+        # Calculate IDF_i for each n_i
+        for word in idfs.keys():
+            idfs[word] = log(N / idfs[word])
+
+        return idfs
+
+    def cosine_sim(self, fileid1, fileid2):
         return 0
 
-    def words(fileids=[]):
+    def cosine_sim_new(self, words, fileid):
         return 0
 
-    # TF-IDF Specific Methods
-    def tfidf(filieid, returnZero=False):
-        return 0
-
-    def tfidfAll(filieid, returnZero=False):
-        return 0
-
-    def tfidfNew(words):
-        return 0
-
-    def idf():
-        return 0
-
-    def cosine_sim(fileid1, fileid2):
-        return 0
-
-    def cosine_sim_new(words, fileid):
-        return 0
-
-    def query(words):
+    def query(self, words):
         return 0
--- program1-tfidf/CorpusReader_TFIDF_test.py
+++ program1-tfidf/CorpusReader_TFIDF_test.py
@@ -1,3 +1,54 @@
-from CorpusReader_TFIDF import CorpusReader_TFIDF
+from nltk.corpus import inaugural, PlaintextCorpusReader
+from CorpusReader_TFIDF import *
 
-tfidf = CorpusReader_TFIDF()
+# Investigating the inaugural corpus
+print('\n> Investigating the inaugural corpus <\n')
+print('*************** 1 ***************')
+print(len(inaugural.words()))
+print('*************** 2 ***************')
+print(inaugural.sents())
+print('*************** 3 ***************')
+print(len(inaugural.sents()))
+print('*************** 4 ***************')
+print(inaugural.fileids())
+print('*************** 5 ***************')
+print(inaugural.sents(['1789-Washington.txt']))
+print('*************** 6 ***************')
+print(len(inaugural.sents(['1789-Washington.txt'])))
+
+# Testing CorpusReader_TFIDF functions
+myCorpus = CorpusReader_TFIDF(inaugural)
+
+print('\n> Testing CorpusReader_TFIDF functions <\n')
+print('*************** 1 ***************')
+print(myCorpus.words())
+print(len(myCorpus.words()))
+print('*************** 2 ***************')
+dict = myCorpus.idf()
+# print(len(myCorpus.idf()))
+
+# print(myCorpus.tfidf('1789-Washington.txt'))
+# print("-----\n")
+# q = myCorpus.tfidfAll()
+# for x in q:
+#    print(x, q[x])
+# print("-----\n")
+# print(myCorpus.cosine_sim('1789-Washington.txt', '2021-Biden.txt')
+# print("-----\n")
+# print(myCorpus.cosine_sim_new(['citizens', 'economic', 'growth', 'economic'],
+# '2021-Biden.txt')
+
+#  This is for testing your own corpus
+#
+#  create a set of text files, store them in a directory specified from 'rootDir' variable
+#
+#
+'''
+rootDir = '/myhomedirectory'   # change that to the directory where the files are
+newCorpus = PlaintextCorpusReader(rootDir, '*')
+tfidfCorpus = CorpusReader_TFIDF(newCorpus)
+q = tfidfCorpus.tfidfAll()
+for x in q:
+   print(x, q[x])
+print("-----\n")
+'''
