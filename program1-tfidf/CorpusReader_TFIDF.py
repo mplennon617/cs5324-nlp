91acaa15badb7792070e329d9fc8c2341ff4c218
mplennon617 <mplennon617@gmail.com>
2023-02-25 20:52:47 -0600

    Project 1 TFIDF logic complete

--- program1-tfidf/CorpusReader_TFIDF.py
+++ program1-tfidf/CorpusReader_TFIDF.py
@@ -38,22 +38,96 @@ class CorpusReader_TFIDF:
         """! Returns set of words from given fileids 
         (default is all docs in the corpus).
 
-        @param fields   List of fileids to read from
+        @param fileids   List of fileids to read from
         @return  Set of words.
         """
         return self.corpus.words(fileids=fileids)
 
     # *************** TF-IDF Specific Methods ***************
     def tfidf(self, fileid, returnZero=False):
-        return 0
+        """! Gets the TF/IDF vector for a given fileid. 
+        (default is all docs in the corpus).
 
-    def tfidfAll(self, fileid, returnZero=False):
-        return 0
+        @param fileid   The fileid to read from
+        @param returnZero If true, return a dictionary of words
+        containing all words with TF/IDF value of zero.
+        @return  TF/IDF vector dictionary.
+        """
+
+        tfidf = {}
+        zeros = {}
+
+        rawDoc = self.raw(fileid)
+        docWords = self.words(fileid)
+
+        # Get IDF
+        idfs = self.get_idf()
+
+        # Get TF-IDF
+        for word in docWords:
+
+            # Calculate TF
+            tf = rawDoc.count(word)
+
+            if self.tf == 'log' and tf != 0:
+                tf = 1 + log(tf)
+
+            # Fill either zeros or tfidf depending on setting
+            tfidf[word] = tf * idfs[word]
+            if returnZero == True and tfidf[word] == 0:
+                zeros[word] = 0
+
+        if returnZero:
+            return zeros
+        return tfidf
+
+    def tfidfAll(self, returnZero=False):
+        """! Gets the TF/IDF vector for all docs in the corpus. 
+        (default is all docs in the corpus).
+
+        @param fileid   The fileid to read from
+        @param returnZero If true, tfidf value will be based on
+        all words with tfidf values of zero.
+        @return  TF/IDF vector dictionary. Key is the fileid
+        and value is the TFIDF calculation for the document.
+        """
+        tfidfall = {}
+
+        for doc in self.corpus.fileids():
+
+            print('ONE')
+            # Get tfidf for this document and merge it into tfidfall
+ 
+            doctfidf = self.tfidf(doc, returnZero=returnZero)
+
+            print('TWO')
+            tfidfall[doc] = doctfidf
+            print('THREE')
+
+        return tfidfall
 
     def tfidfNew(self, words):
-        return 0
 
-    def idf(self):
+        tfidf = {}
+
+        # Get IDF
+        idfs = self.get_idf()
+
+        # Get TF-IDF
+        for word in words:
+
+            # Calculate TF
+            tf = words.count(word)
+
+            if self.tf == 'log' and tf != 0:
+                tf = 1 + log(tf)
+
+            # Fill either zeros or tfidf depending on setting
+            tfidf[word] = tf * idfs[word]
+
+        return tfidf
+
+    def get_idf(self):
         """! Return IDF of each term in the corpus as a dictionary. 
         Key is the words, value is the IDF
 
@@ -61,15 +135,20 @@ class CorpusReader_TFIDF:
         """
 
         idfs = {}
-        print('We\'re in!')
 
         # Find N
         N = len(self.corpus.fileids())
 
         # Calculate Document Frequencies (n_i)
         for doc in self.corpus.fileids():
-            for word in self.words('doc'):
-                idfs[word] = idfs.get(word, default=0) + 1
+            appeared = {}
+            # print('ONE')
+            # For each word, if it did not yet appear in the doc, add one to doc frequency
+            for word in self.words(doc):
+                if word not in appeared:
+                    # print('TWO',word)
+                    idfs[word] = idfs.get(word, 0) + 1
+                    appeared[word] = True
 
         # Calculate IDF_i for each n_i
         for word in idfs.keys():
